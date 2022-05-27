d8da3a9c5a070fb3f76e0c981c284a5d0917f6f5
Akifcan Özbulut <56379702+Afikcan@users.noreply.github.com>
2022-05-27 09:58:30 +0300

    update name

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Akifcan Özbulut'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Akifcan Özbulut'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Akifcan Özbulut'
     })
 })
 
@@ -86,7 +86,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Akifcan Özbulut',
         errorMessage: 'Help article not found.'
     })
 })
@@ -94,7 +94,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Akifcan Özbulut',
         errorMessage: 'Page not found.'
     })
 })
